fd902efb16f33d421606eacf573d07103ee94920
IsabellaMoura <70759293+IsabellaMoura@users.noreply.github.com>
2021-12-10 05:05:43 -0300

    Add files via upload

---
+++ Logica_De_Programacao/Java/DoWhile.java
@@ -0,0 +1,13 @@
+package exemplo;
+
+public class DoWhile {
+	public static void main(String[] args) {
+
+		int contadora=0;
+		do {
+			System.out.println("Jajá");
+			contadora = contadora+1;
+		}while(contadora<5);
+	}
+
+}
---
+++ Logica_De_Programacao/Java/While.java
@@ -0,0 +1,12 @@
+package exemplo;
+
+public class While {
+public static void main(String[] args) {
+int contador=10;
+while(contador<8) {
+	System.out.println("Osasco");
+	contador=contador+1;
+	//contador++;
+}
+}
+}
